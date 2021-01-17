2a35e3cbdcd421042a96d31b514f28e2683cd2ea
Matt Rosanio <matt.rosanio@gmail.com>
2021-01-17 17:31:31 -0500

    Initial commit, can generate flat map, player can look and move

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] int MapWidth = 16;
+    [SerializeField] int MapDepth = 16;
+    [SerializeField] GameObject Dirt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        MeshRenderer CubeRenderer = Dirt.GetComponent<MeshRenderer>();
+        float CubeWidth = CubeRenderer.bounds.size.x;
+        float CubeDepth = CubeRenderer.bounds.size.z;
+
+        for (int x = 0; x < MapWidth; x++)
+        {
+            for (int z = 0; z < MapDepth; z++)
+            {
+                Vector3 CubePosition = new Vector3(CubeWidth * x, 0, CubeDepth * z);
+                Instantiate(Dirt, CubePosition, Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] GameObject Camera;
+    [SerializeField] bool InvertY = false;
+    [SerializeField] float MouseSensitivity = 1;
+    [SerializeField] float PlayerWalkSpeed = 10;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        UpdateCameraRotation();
+
+        Vector3 Forward = new Vector3(Camera.transform.forward.x, 0, Camera.transform.forward.z);
+        if (Input.GetKey("w"))
+        {
+            transform.position += Forward * Time.deltaTime * PlayerWalkSpeed;
+        }
+        else if (Input.GetKey("s"))
+        {
+            transform.position -= Forward * Time.deltaTime * PlayerWalkSpeed;
+        }
+
+        Vector3 Right = new Vector3(Camera.transform.right.x, 0, Camera.transform.right.z);
+        if (Input.GetKey("d"))
+        {
+            transform.position += Right * Time.deltaTime * PlayerWalkSpeed;
+        }
+        else if (Input.GetKey("a"))
+        {
+            transform.position -= Right * Time.deltaTime * PlayerWalkSpeed;
+        }
+    }
+
+    private void UpdateCameraRotation()
+    {
+        // Rotate about global y
+        float XDelta = Input.GetAxis("Mouse X") * MouseSensitivity;
+        Quaternion q = Quaternion.AngleAxis(XDelta, Vector3.up);
+        Camera.transform.rotation = q * Camera.transform.rotation;
+
+        // Rotate about local x
+        float YDelta = Input.GetAxis("Mouse Y") * MouseSensitivity;
+        if (!InvertY) YDelta = -YDelta;
+        Camera.transform.Rotate(new Vector3(YDelta, 0, 0));
+    }
+}
